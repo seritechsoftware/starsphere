eb0ab0a2b8a25d5aeb5c3d45057217a98ef96889
seritechsoftware <dev@seritechsoftware.com>
2016-10-15 20:31:39 -0400

    first commit - test

--- starsphere/Program.cs
+++ starsphere/Program.cs
@@ -14,7 +14,7 @@ namespace starsphere
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new openingwindow())
                 game.Run();
         }
     }
--- starsphere/openingwindow.cs
+++ starsphere/openingwindow.cs
@@ -7,12 +7,12 @@ namespace starsphere
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class openingwindow : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public openingwindow()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
