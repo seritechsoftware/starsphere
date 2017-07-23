aa6bdd11e0bc26ded80e42633057e8ec29266e0b
seritechsoftware <dev@seritechsoftware.com>
2017-07-23 09:48:00 -0400

    Rearranged Folder Makeup

---
+++ starsphere/Game Logic/GalaxyCatalog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starsphere.Game_Logic
+{
+    class GalaxyCatalog
+    {
+    }
+}
